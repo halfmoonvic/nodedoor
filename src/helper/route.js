cebfd4d232e70625cf716d9850ad4092d417b468
cs-mac <halfmoonvic@gmail.com>
2018-05-29 09:25:08 +0800

    添加 handlebars 模板

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,7 +1,14 @@
+const Handlebars = require('handlebars')
+const path = require('path')
 const fs = require('fs')
 const promisify = require('util').promisify
 const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
+const config = require('../config/defaultConfig')
+
+const tplPath = path.join(__dirname, '../template/dir.tpl')
+const source = fs.readFileSync(tplPath)
+const template = Handlebars.compile(source.toString())
 
 module.exports = async (req, res, filePath) => {
   try {
@@ -10,23 +17,23 @@ module.exports = async (req, res, filePath) => {
     if (stats.isFile()) {
       res.statusCode = 200
       res.setHeader('Content-Type', 'text/plain')
-      // fs.readFile(filePath, (err, data) => {
-      //   res.end(data)
-      // })
-      fs.createStream(filePath).pipe(res)
+      fs.createReadStream(filePath).pipe(res)
     } else if (stats.isDirectory()) {
       const files = await readdir(filePath)
       res.statusCode = 200
-      res.setHeader('Content-Type', 'text/plain')
-      res.end(files.join(','))
+      res.setHeader('Content-Type', 'text/html')
 
-      // fs.readdir(filePath, (err, files) => {
-      //   res.statusCode = 200
-      //   res.setHeader('Content-Type', 'text/plain')
-      //   res.end(files.join(','))
-      // })
+      const dir = path.relative(config.root, filePath)
+      const data = {
+        title: path.basename(filePath),
+        dir: dir ? `/${dir}` : '',
+        files
+      }
+      console.log(data)
+      res.end(template(data))
     }
   } catch (e) {
+    console.log(e)
     res.statusCode = 404
     res.setHeader('Content-Type', 'text/plain')
     res.end(`${filePath} is not a directory`)
---
+++ src/template/dir.tpl
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>{{title}}</title>
+  <style type="text/css">
+    body {
+      margin: 30px;
+    }
+
+    a {
+      display: block;
+      font-size: 20px;
+    }
+
+  </style>
+</head>
+
+<body>
+
+  {{#each files}}
+  <a href="{{../dir}}/{{this}}">{{this}}</a> {{/each}}
+
+</body>
+
+</html>
