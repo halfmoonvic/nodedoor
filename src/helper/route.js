28def84e24d0d7652d81f92e14e31ea25f258373
cs-mac <halfmoonvic@gmail.com>
2018-06-03 10:38:25 +0800

    feat: 添加 range 请求数据 curl -r 0-3 -i http://127.0.0.1:9527/package.json

--- src/helper/route.js
+++ src/helper/route.js
@@ -8,6 +8,7 @@ const config = require('../config/defaultConfig')
 
 const mime = require('../helper/mime')
 const compress = require('../helper/compress')
+const range = require('../helper/range')
 
 const tplPath = path.join(__dirname, '../template/dir.tpl')
 const source = fs.readFileSync(tplPath)
@@ -18,13 +19,25 @@ module.exports = async (req, res, filePath) => {
     const stats = await stat(filePath)
 
     if (stats.isFile()) {
+      // 判断文件类型，添加 Content-Type 文件类型
       const contentType = mime(filePath)
-      res.statusCode = 200
       res.setHeader('Content-Type', contentType)
-      let rs = fs.createReadStream(filePath)
+      res.statusCode = 200
+
+      // range 请求
+      let rs
+      const { code, start, end } = range(stats.size, req, res)
+      if (code === 200) {
+        res.statusCode = 200
+        rs = fs.createReadStream(filePath)
+      } else {
+        res.statusCode = 206
+        rs = fs.createReadStream(filePath, { start, end })
+      }
       if (filePath.match(config.compress)) {
         rs = compress(rs, req, res)
       }
+
       rs.pipe(res)
     } else if (stats.isDirectory()) {
       const files = await readdir(filePath)
