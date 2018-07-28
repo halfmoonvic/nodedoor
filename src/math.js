2574013e595586151fe8f075b27d5caa7182f105
cs-mac <halfmoonvic@gmail.com>
2018-07-28 11:59:38 +0800

    istanbul 测试覆盖率

--- src/math.js
+++ src/math.js
@@ -1,9 +1,19 @@
 module.exports = {
-  add: (...args) => {
-    return args.reduce((a, b) => a + b)
-  },
+    add: (...args) => {
+        return args.reduce((a, b) => a + b)
+    },
 
-  mul: (...args) => {
-    return args.reduce((a, b) => a * b)
-  }
+    mul: (...args) => {
+        return args.reduce((a, b) => a * b)
+    },
+
+    cover: (a, b) => {
+        if (a > b) {
+            return a - b
+        } else if (a == b) {
+            return a + b
+        } else {
+            return 'hfaeh'
+        }
+    }
 }
--- test/mocha.js
+++ test/mocha.js
@@ -1,5 +1,5 @@
 const { should, expect, assert } = require('chai')
-const { add, mul } = require('../src/math')
+const { add, mul, cover } = require('../src/math')
 
 // mocha 自己并不包含断言库的
 // mocha 是一个 render? 不需要依赖，会自己注入
@@ -18,6 +18,21 @@ describe('#math', () => {
     })
     it('should return 8 when 2 * 4', () => {
       assert(mul(2, 4), 8)
+    })
+  })
+
+  // istanbul 测试用例 测试每个条件的通过。看看是否覆盖全面
+  describe('cover', () => {
+    it('should return a < b', () => {
+      assert(cover(2, 3), 'hfaeh')
+    })
+
+    it('should return a > b', () => {
+      assert(cover(3, 2), 1)
+    })
+
+    it('should return a > b', () => {
+      assert(cover(2, 2), 0)
     })
   })
 })
