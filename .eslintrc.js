742f24aa2713f81a21e1611735c903ecce203206
cs-mi <halfmoonvic@gmail.com>
2018-08-07 22:33:16 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,38 @@
 module.exports = {
+  root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    parser: 'babel-eslint'
+    ecmaVersion: 6,
+    sourceType: 'module'
   },
   env: {
+    browser: true,
+    node: true,
     es6: true,
-    node: true
+    commonjs: true
   },
+  globals: {},
+  plugins: [],
   extends: 'eslint:recommended',
   rules: {
-    // allow async-await
-    'space-before-function-paren': ["warn", "never"],
-    'generator-star-spacing': 'off',
-    'spaced-comment': 'off',
-    'indent': ['warn', 2],
-    'no-unused-vars': 'warn',
-    'semi': ['warn', 'never'],
-    // 不允许多个空行
-    'no-multiple-empty-lines': ["warn", { "max": 2}],
-    // 要求或禁止使用拖尾逗号
-    "comma-dangle": ["warn", "never"],
-    // 禁止出现多个空格
-    'no-multi-spaces': "off",
-    // allow debugger during development
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    // 单引号，双引号
-    'quotes': ['error', 'single'],
-    "no-console": 'off',
+    eqeqeq: 1,
+    'no-console': 0,
+    'space-before-function-paren': [
+      1,
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+    'generator-star-spacing': 0,
+    'spaced-comment': 1,
+    indent: [1, 2],
+    'no-unused-vars': 1,
+    semi: [1, 'never'],
+    'no-multiple-empty-lines': [1, { max: 2 }],
+    'comma-dangle': [1, 'never'],
+    'no-multi-spaces': 1,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 0 : 1
   }
 }
